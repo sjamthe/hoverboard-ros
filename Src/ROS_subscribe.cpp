d786911e8032443a1d10c1a604953beea05a115f
Shirish Jamthe <sjamthe@yahoo.com>
2019-09-11 19:32:56 -0700

    position callback works in both directions

--- Src/ROS_subscribe.cpp
+++ Src/ROS_subscribe.cpp
@@ -9,83 +9,91 @@
 
 #include "ROS_subscribe.h"
 
+#define SAFETY_MS 2000
+#define POSITION_PWM 50
+
 extern "C" 
 {
 	void wheels_pwm_set(void);
 }
 
 static sensor_msgs::JointState wheelPositions;
-uint32_t wheelPositionsAt;
+uint32_t wheelInputAt; //MS at which we received the input callback
 
 extern int pwms[2];
 extern volatile WHEEL_POSN_STRUCT wheel_posn[2];
 
 long ticksTarget[2] = {0,0}; //ticks target set by ROS
 float rpmTarget[2] = {0,0}; //velocity/rpm target set by ROS
+int motionDirection[2]; //Were we asked to move forward or back?
 
 void wheels_cmd_cb(unsigned char* msg)
 {
-	wheelPositionsAt = HAL_GetTick();
-	wheelPositions.deserialize(msg);  
-	printf("%lu: New wheels_cmd_cb received\n",HAL_GetTick());
+	wheelInputAt = HAL_GetTick();
+	wheelPositions.deserialize(msg); 
+  for (int i=0; i < wheelPositions.name_length && i<2; i++)
+  {
+    ticksTarget[i] = long(wheelPositions.position[i]) + wheel_posn[i].ticks; 
+    //
+    if(ticksTarget[i]*wheel_posn[i].direction >= wheel_posn[i].ticks*wheel_posn[i].direction)
+    {
+      motionDirection[i] = 1*wheel_posn[i].direction;
+    }
+    else
+    {
+      motionDirection[i] = -1*wheel_posn[i].direction;
+    }
+    printf("%lu: wheels_cmd_cb tickTarget=%ld, dir=%d\n",wheelInputAt,ticksTarget[i],motionDirection[i] );
+  }
 }
 
 void wheels_pwm_set()
 {
   uint32_t now = HAL_GetTick();
 
-  //safety stop if no new update in 1000ms
-  if((now - wheelPositionsAt) > 2000)
+  //safety stop if no new update in MS
+  if((now - wheelInputAt) > SAFETY_MS)
   {
-    for (int i=0;i<2;i++)
+    for (int i=0; i<2;i++)
     {
-	  if(pwms[i] != 0) {
-	  	printf("%lu:LIMIT reached. set speed to 0 , %lu, %lu for pwms[%d]=%d ticks=%ld\n",
-	  			now, wheelPositionsAt, (now - wheelPositionsAt), i, pwms[i], wheel_posn[i].ticks);
-	  }
-      pwms[i] = 0;
-      ticksTarget[i] = 0;
-      rpmTarget[i] = 0;
-	  wheelPositionsAt = 0; //reset clock;
+      if(pwms[i] != 0) {
+        printf("%lu:LIMIT reached. set speed to 0 , %lu, %lu for pwms[%d]=%d ticks=%ld\n",
+            now, wheelInputAt, (now - wheelInputAt), i, pwms[i], wheel_posn[i].ticks);
+      }
+      pwms[i] = 0; //stop motor
+	    wheelInputAt = 0; //reset clock;
     }
     return;
   }
-
-  //Get wheel positions and set the values
-  if(wheelPositions.name_length == 2) 
+  else if (wheelInputAt > 0) //set PWM based on input from ros
   {
-    for (int i=0;i<2; i++)
+    //check wheelPositions.name_length <= 2 for safety
+    for (int i=0;i < wheelPositions.name_length && i < 2; i++)
     {
-      //If position target was set to 0 set a new target, if we met/passed it then reset it
-      if(wheelPositions.position[i] != 0)
+      //If the current position exceed target (with right direction) then stop motor
+      if(ticksTarget[i]*motionDirection[i] <= wheel_posn[i].ticks*motionDirection[i])
       {
-        if(ticksTarget[i] == 0)
+        // We met the target
+        printf("%lu:Target[%d] met %ld <= %ld pwms=%d\n",now,i,
+              ticksTarget[i]*motionDirection[i],wheel_posn[i].ticks*motionDirection[i],pwms[i] );
+       
+        // stop motor
+        pwms[i] = 0;
+        wheelInputAt = 0;
+      }
+      else if(ticksTarget[i] != 0 && pwms[i] == 0) //new target was set
+      {
+        if(motionDirection[i] == 1) // go forward
         {
-          //Set new target to current + delta requested
-          ticksTarget[i] = long(wheelPositions.position[i]*wheel_posn[i].direction) + wheel_posn[i].ticks;
-          if(ticksTarget[i] >= wheel_posn[i].ticks)
-          {
-            // go forward
-            pwms[i] = 100*wheel_posn[i].direction;
-          }
-          else
-          {
-            // go back
-            pwms[i] = -100*wheel_posn[i].direction;
-          }
-		      printf("%lu:Target set for [%d] %ld=%d+%d\n",now,i,ticksTarget[i],
-		  			long(wheelPositions.position[i]), wheel_posn[i].ticks);
+          pwms[i] = POSITION_PWM*wheel_posn[i].direction;
+          printf("%lu:Target[%d] set for forward %ld <= %ld, pwms=%d\n",now,i,
+              ticksTarget[i]*motionDirection[i],wheel_posn[i].ticks*motionDirection[i],pwms[i] );
         }
-        else if((wheel_posn[i].ticks*wheel_posn[i].direction) >= 
-                (ticksTarget[i]*wheel_posn[i].direction))
+        else // go back
         {
-          // We met the target
-          printf("%lu:Target met for %d %ld >= %ld\n",now,i,
-                wheel_posn[i].ticks, ticksTarget[i]);
-          
-          ticksTarget[i] = 0;
-          pwms[i] = 0;
-          wheelPositionsAt = 0; //reset clock;
+          pwms[i] = -POSITION_PWM*wheel_posn[i].direction;
+          printf("%lu:Target[%d] set for backward %ld <= %ld, pwms=%d\n",now,i,
+              ticksTarget[i]*motionDirection[i],wheel_posn[i].ticks*motionDirection[i],pwms[i] );
         }
       }
       // For velocity target we need pwm
@@ -101,11 +109,11 @@ void wheels_pwm_set()
         {
           pwms[i] = MIN(PWM_LIMIT,int(wheelPositions.effort[i])); 
         }
-		printf("%lu:New PWM set for pwms[%d]=%d\n",now,i,pwms[i]);
+        printf("%lu:New PWM set for pwms[%d]=%d\n",now,i,pwms[i]);
       }     
-    }
-  }
-}
+    } // end of for
+  } //end of if-else
+} //end of function
 
 uint32_t rosSubscribeWheelsCmd(ros::NodeHandle *nh)
 {
